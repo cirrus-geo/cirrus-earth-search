bec97739a18d00512c5c37b206b80b876e16d2aa
Matthew Hanson <matt.a.hanson@gmail.com>
2020-10-15 12:08:59 -0400

    update sentinel to stac creation code

--- tasks/sentinel-to-stac/lambda_function.py
+++ tasks/sentinel-to-stac/lambda_function.py
@@ -23,22 +23,22 @@ logger.addHandler(logging.StreamHandler())
 DATA_BUCKET = getenv('CIRRUS_DATA_BUCKET')
 
 
+
+def fetch_metadata(url):
+    resp = requests.get(url, stream=True)
+    if resp.status_code != 200:
+        msg = f"sentinel-to-stac: failed fetching {url} ({resp.text})"
+        logger.error(msg)
+        logger.error(format_exc())
+        raise InvalidInput(msg)
+    else:
+        return json.loads(resp.text)
+
+
 def lambda_handler(payload, context={}):
     logger.debug('Payload: %s' % json.dumps(payload))
 
     catalog = Catalogs.from_payload(payload)[0]
-
-    # TODO - make this more general for more items/collections
-    assert(len(catalog['features']) == 1)
-
-    # configuration options
-    #config = catalog['process']['functions'].get('sentinel-to-stac', {})
-    #output_options = catalog['process'].get('output_options', {})
-    #output_credentials = output_options.get('credentials', {})
-
-    # this process assumes single output collection, as it's just converting from original Sentinel to STAC for 1 scene
-    #output_collection = list(catalog['process']['output_options']['collections'].keys())[0]
-    output_collection = 'sentinel-s2-l2a'
 
     items = []
     # get metadata
@@ -51,60 +51,55 @@ def lambda_handler(payload, context={}):
 
     # TODO - handle getting from s3 as well as http?
     # get metadata
-    resp = requests.get(url, stream=True)
-    if resp.status_code != 200:
-        msg = f"sentinel-to-stac: failed fetching {url} for {catalog['id']} ({resp.text})"
-        logger.error(msg)
-        logger.error(format_exc())
-        raise InvalidInput(msg)
-    metadata = json.loads(resp.text)
+    metadata = fetch_metadata(url)
 
-    if 'tileDataGeometry' in metadata:
-        coords = metadata['tileDataGeometry'].get('coordinates', [[]])
-        if len(coords) == 1 and len(coords[0]) == 0:
+    #if 'tileDataGeometry' in metadata:
+    #    coords = metadata['tileDataGeometry'].get('coordinates', [[]])
+    #    if len(coords) == 1 and len(coords[0]) == 0:
             # if empty list then drop tileDataGeometry, will try to get from L1C
-            metadata.pop('tileDataGeometry')
+    #        metadata.pop('tileDataGeometry')
 
-    # if sentinel-s2-l2a we need to get cloud cover from sentinel-s2-l1c
-    # so go ahead and publish that one as well
-    if output_collection == 'sentinel-s2-l2a':
+    # need to get cloud cover from sentinel-s2-l1c since missing from l2a so fetch and publish l1c as well
+    try:
         _url = url.replace('sentinel-s2-l2a', 'sentinel-s2-l1c')
-        resp = requests.get(_url, stream=True)
-        l1c_present = False
-        if resp.status_code != 200:
-            msg = f"sentinel-to-stac: failed fetching {_url} for {catalog['id']} ({resp.text})"
-            logger.error(msg)
-        else:
-            l1c_present = True
-            logger.debug(f"sentinel-s2-l1c request response: {resp}")
-            _metadata = json.loads(resp.text)
+        l1c_metadata = fetch_metadata(_url)
+    except InvalidInput:
+        l1c_metadata = None
 
-            # tileDataGeometry in L2A but not in L1C
-            if 'tileDataGeometry' not in _metadata:
-                if 'tileDataGeometry' in metadata:
-                    _metadata['tileDataGeometry'] = metadata['tileDataGeometry']
-                else:
-                    msg = f"sentinel-to-stac: no valid data geometry available for {catalog['id']}"
-                    logger.error(msg)
-                    raise InvalidInput(msg)
+    if l1c_metadata is not None:
+        # tileDataGeometry in L2A but not in L1C
+        if 'tileDataGeometry' not in l1c_metadata:
+            if 'tileDataGeometry' in metadata:
+                l1c_metadata['tileDataGeometry'] = metadata['tileDataGeometry']
+            else:
+                msg = "sentinel-to-stac: no valid data geometry available"
+                logger.error(msg)
+                raise InvalidInput(msg)
 
-            _item = sentinel_s2_l1c(_metadata, base_url.replace('sentinel-s2-l2a', 'sentinel-s2-l1c'))
+        try:
+            _item = sentinel_s2_l1c(l1c_metadata, base_url.replace('sentinel-s2-l2a', 'sentinel-s2-l1c'))
             for a in ['thumbnail', 'info', 'metadata']:
                 _item['assets'][a]['href'] = _item['assets'][a]['href'].replace('s3:/', 'https://roda.sentinel-hub.com')
             # if dataCoveragePercentage not in L1 data, try getting from L2
-            if 'dataCoveragePercentage' not in _metadata and 'dataCoveragePercentage' in metadata:
+            if 'dataCoveragePercentage' not in l1c_metadata and 'dataCoveragePercentage' in metadata:
                 _item['properties']['sentinel:data_coverage'] = float(metadata['dataCoveragePercentage'])
             items.append(_item)
+        except Exception as err:
+            msg = f"sentinel-to-stac: failed creating L1C STAC ({err})"
+            logger.error(msg)
+            logger.error(format_exc())
+            raise InvalidInput(msg)
 
-            # use L1C cloudyPixelPercentage
-            metadata['cloudyPixelPercentage'] = _metadata['cloudyPixelPercentage']
+        # use L1C cloudyPixelPercentage
+        metadata['cloudyPixelPercentage'] = l1c_metadata['cloudyPixelPercentage']
 
-            # tileDataGeometry in L1C but not L2A
-            if 'tileDataGeometry' not in metadata and 'tileDataGeometry' in _metadata:
-                metadata['tileDataGeometry'] = _metadata['tileDataGeometry']
+        # tileDataGeometry in L1C but not L2A
+        if 'tileDataGeometry' not in metadata and 'tileDataGeometry' in l1c_metadata:
+            metadata['tileDataGeometry'] = l1c_metadata['tileDataGeometry']
 
+    # tileDataGeometry not available
     if 'tileDataGeometry' not in metadata:
-        msg = f"sentinel-to-stac: no valid data geometry available for {catalog['id']}"
+        msg = f"sentinel-to-stac: no valid data geometry available"
         logger.error(msg)
         raise InvalidInput(msg)
 
@@ -113,20 +108,20 @@ def lambda_handler(payload, context={}):
         for a in ['thumbnail', 'info', 'metadata']:
             item['assets'][a]['href'] = item['assets'][a]['href'].replace('s3:/', 'https://roda.sentinel-hub.com')
 
-        if l1c_present:
+        if l1c_metadata is not None:
             item['properties']['sentinel:valid_cloud_cover'] = True
         else:
             item['properties']['sentinel:valid_cloud_cover'] = False
         items.append(item)
     except Exception as err:
-        msg = f"sentinel-to-stac: failed creating STAC for {catalog['id']} ({err})"
+        msg = f"sentinel-to-stac: failed creating STAC ({err})"
         logger.error(msg)
         logger.error(format_exc())
-        raise Exception(msg)
+        raise InvalidInput(msg)
 
     # discard if crossing antimeridian
     if item['bbox'][2] - item['bbox'][0] > 300:
-        msg = f"{item['id']} crosses antimeridian, discarding"
+        msg = "sentinel-to-stac: crosses antimeridian, discarding"
         logger.error(msg)
         raise InvalidInput(msg)
 
